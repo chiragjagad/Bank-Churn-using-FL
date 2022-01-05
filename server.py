aa0d6f071c6098bb3178f98c07c1b60e31c8503b
Chirag Jagad <chiragjagad08@gmail.com>
2022-01-05 13:47:29 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,13 @@
+import flwr as fl
+
+# Start Flower server for three rounds of federated learning
+if __name__ == "__main__":
+
+    # Create strategy
+    strategy = fl.server.strategy.FedAvg(
+        fraction_eval=1,
+        min_eval_clients=2,
+        min_available_clients=2,
+    )
+    fl.server.start_server("127.0.0.1:8080", config={
+                           "num_rounds": 5}, strategy=strategy)
